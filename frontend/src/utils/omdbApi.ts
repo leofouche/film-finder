d079199b263fae10dbec472ae07c57e465b03bbe
leofouche <62493023+leofouche@users.noreply.github.com>
2025-08-05 23:29:15 +1000

    https request

--- frontend/src/utils/omdbApi.ts
+++ frontend/src/utils/omdbApi.ts
@@ -1,5 +1,5 @@
 const OMDB_API_KEY = "1cbd3ca7";
-const OMDB_BASE_URL = 'http://www.omdbapi.com/';
+const OMDB_BASE_URL = 'https://www.omdbapi.com/';
 
 export interface OMDbResponse {
   Title: string;
